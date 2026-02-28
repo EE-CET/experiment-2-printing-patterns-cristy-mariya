c38f90d59afbdc4787e23db95d7f9f5c23d5ef9d
cristy-mariya <cristymariya16@gmail.com>
2026-02-28 23:33:03 +0530

    changed code

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
+  public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      for(int i=1;i<=n;i++){
+         System.out.print("*");}
+      System.out.println();
+      for(int i=1;i<n;i++){
+        System.out.println("*");
+}
+}
     
 }
